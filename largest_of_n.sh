d2c55b5fd0ba77c91b8ce64c70a66561e9913993
tanushekar <tanushekar.015@gmail.com>
2025-09-21 00:03:03 +0530

    Create largest_of_n.sh

---
+++ largest_of_n.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Enter the size of the array: "
+read n
+
+declare -A arr
+
+echo "Enter the elements of the array"
+for ((i=0;i<n;i++))                         
+do
+  read arr["$i"]                     
+done
+
+max=${arr[0]}                          
+
+for ((i=0;i<n;i++))
+do
+  if [[ $max -lt ${arr[$i]} ]]         
+  then 
+      max=${arr[$i]}
+  fi
+done
+
+echo "The largest of the given numbers is $max"
